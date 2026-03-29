697ad225a921c9c57fb9556afa0bd1cb2a7e2574
aaronmaldonado1 <uamava@gmail.com>
2026-03-28 21:07:35 -0300

    Update markets.js

--- api/markets.js
+++ api/markets.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  const response = await fetch('https://gamma-api.polymarket.com/markets?limit=100&active=true&closed=false&search=elon+musk+tweets');
+  const response = await fetch('https://gamma-api.polymarket.com/events?limit=100&active=true&closed=false&search=elon+musk+tweets');
   const data = await response.json();
   res.json(data);
 }
